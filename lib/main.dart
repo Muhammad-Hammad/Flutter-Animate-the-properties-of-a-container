c57e9419495226a49d31dc681f1beb2735fec061
Syed Muhammad Hammad Ghani <hammy.pk30@gmail.com>
2021-08-25 19:47:13 +0500

    done

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: AnimatedContainerApp());
+  }
+}
+
+class AnimatedContainerApp extends StatefulWidget {
+  const AnimatedContainerApp({Key? key}) : super(key: key);
+
+  @override
+  _AnimatedContainerAppState createState() => _AnimatedContainerAppState();
+}
+
+class _AnimatedContainerAppState extends State<AnimatedContainerApp> {
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Animated Container'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: Duration(
+            seconds: 1,
+          ),
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(color: _color, borderRadius: _borderRadius),
+          curve: Curves.fastOutSlowIn,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            final random = Random();
+            _width = random.nextInt(300).toDouble();
+            _height = random.nextInt(300).toDouble();
+            _color = Color.fromRGBO(random.nextInt(256), random.nextInt(256),
+                random.nextInt(256), 1);
+            _borderRadius =
+                BorderRadius.circular(random.nextInt(100).toDouble());
+          });
+        },
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
